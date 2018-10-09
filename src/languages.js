dd29a03cfed4fd4f379d05805badb17ad560b53a
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2018-10-09 13:42:55 +0200

    Fix navigator.languages issue with internet explorer

--- src/languages.js
+++ src/languages.js
@@ -4,7 +4,10 @@ console.log(navigator.language, navigator.languages)
 const default_lang = 'en'
 
 export default function(obj) {
-  let lang = navigator.languages
+  let lang = [navigator.language]
+  if (navigator.languages) {
+    lang = navigator.languages
+  }
   let key = default_lang
   for (let i = 0; i < lang.length; i++) {
     if (obj[lang[i]]) {
