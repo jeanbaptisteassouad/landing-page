80dd7944fdac4f08153049be0da6009735210a5b
Jean-Baptiste Assouad <jean.baptiste@assouad.com>
2018-10-09 18:40:07 +0200

    add app image bin to landing for v9

--- src/platforms.js
+++ src/platforms.js
@@ -10,5 +10,9 @@ export default [
     name:'Windows 32 bit',
     url_end:'windows32/archifiltre32.exe',
   },
+  {
+    name:'Linux 64 bit',
+    url_end:'linux64/archifiltre64.tar.gz',
+  },
 ]
 
